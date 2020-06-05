e860ffbf65da70d528c6526360c2793627ea28a3
XMRig <support@xmrig.com>
2020-06-05 21:52:39 +0700

    v6.1.0

--- src/version.h
+++ src/version.h
@@ -28,13 +28,13 @@
 #define APP_ID        "xmrig-cuda"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig CUDA plugin"
-#define APP_VERSION   "6.0.0"
+#define APP_VERSION   "6.1.0"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2020 xmrig.com"
 
 #define APP_VER_MAJOR  6
-#define APP_VER_MINOR  0
+#define APP_VER_MINOR  1
 #define APP_VER_PATCH  0
 
 #define API_VERSION    3
