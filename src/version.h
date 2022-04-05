8feec79fa98fe6431d80d20a9430424498b7dc00
XMRig <support@xmrig.com>
2022-04-06 01:56:35 +0700

    v6.17.1-dev

--- src/version.h
+++ src/version.h
@@ -22,14 +22,14 @@
 #define APP_ID        "xmrig-cuda"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig CUDA plugin"
-#define APP_VERSION   "6.17.0"
+#define APP_VERSION   "6.17.1-dev"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2022 xmrig.com"
 
 #define APP_VER_MAJOR  6
 #define APP_VER_MINOR  17
-#define APP_VER_PATCH  0
+#define APP_VER_PATCH  1
 
 #define API_VERSION    4
 
